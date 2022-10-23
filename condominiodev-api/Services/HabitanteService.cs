9789c443f122c5e4ac0cca8342f169ab5c51acbe
bruna simoes moita <bruna.simoesmoita@gmail.com>
2022-10-23 17:19:03 -0300

    add service interface

---
+++ condominiodev-api/Services/HabitanteService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using condominiodev_api.Interfaces.Repository;
+using condominiodev_api.Interfaces.Service;
+
+namespace condominiodev_api.Services
+{
+    public class HabitanteService : IHabitanteService
+    {
+        private readonly IHabitanteRepository _habitanteRepositoy;
+
+        public HabitanteService (IHabitanteRepository habitanteRepository)
+        {
+            _habitanteRepositoy = habitanteRepository;
+        }
+
+        public void Delete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
